715672ba6d9e94b8d3a574a71b9f037fa5859759
Musab Erdemir <musaberdemirmc2@gmail.com>
2022-12-08 16:00:51 +0300

    Added proj files

---
+++ Ball.js
@@ -0,0 +1,44 @@
+class Ball {
+    constructor(x, y, rad) {
+        this.rad = rad;
+        this.b = 0.8;
+        this.pos = createVector(x, y);
+        this.v = createVector(5, 0);
+        this.gravity = createVector(0, 0.1);
+    }
+
+    physics() {
+        fill(255, 255, 0);
+        stroke(0);
+        strokeWeight(5);
+        circle(this.pos.x, this.pos.y, this.rad)
+
+        this.v.add(this.gravity);
+        this.pos.add(this.v);
+
+        if (this.pos.y + this.rad / 2 > height) {
+            this.pos.y = height - this.rad / 2
+            this.v.x *= +this.b;
+            this.v.y *= - this.b;
+        } else if (this.pos.y + this.rad / 2 < this.rad / 2) {
+            this.pos.y = this.rad / 2
+            this.v.y *= - 1;
+        }
+
+        if (this.pos.x > width - this.rad / 2) {
+            this.v.x *= - this.b;
+            this.pos.x = width - this.rad / 2
+        }
+        else if (this.pos.x < this.rad / 2) {
+            this.v.x *= - this.b;
+            this.pos.x = this.rad / 2
+        }
+    }
+
+    thrust(vector) {
+        this.v.add(vector);
+    }
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,65 @@
+let yoffset = 100;
+let random = Math.ceil(Math.random() * 9) * (Math.round(Math.random()) ? 1 : -1) / 10;
+let multiplier = 15;
+let ball = [];
+let mousePos;
+let forceDir;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0, 10, 40)
+	ball.push(new Ball(yoffset, yoffset, 50));
+	mousePos = createVector(0, 0);
+
+}
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+function draw() {
+	frameRate(144);
+	background(0, 10, 40)
+	if (ball.length != 0) {
+		ball.forEach(element => {
+			element.physics();
+		});
+	}
+
+}
+
+
+
+function keyPressed() {
+	let vector = createVector(0, -3);
+	if (ball.length != 0) {
+		ball.forEach(element => {
+			element.thrust(vector);
+		});
+	}
+}
+
+
+
+function mousePressed() {
+	let firstPos = createVector(mouseX, mouseY);
+	mousePos.add(firstPos)
+}
+
+
+function mouseReleased() {
+	let first = [width / mousePos.x, height / mousePos.y]
+	mousePos.x = 0;
+	mousePos.y = 0;
+	let second = [width / mouseX, height / mouseY]
+	console.log("first: " + first + "\n" + "second: " + second);
+	let diffArray = new Array(2);
+	diffArray[0] = first[0] - second[0];
+	diffArray[1] = first[1] - second[1];
+	console.log("\ndiff: " + diffArray);
+	forceDir = createVector(multiplier * diffArray[0], multiplier * diffArray[1])
+
+	if (ball.length != 0) {
+		ball.forEach(element => {
+			element.thrust(forceDir);
+		});
+	}
+}
